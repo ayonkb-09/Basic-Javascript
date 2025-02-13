9bc43fd93b65e38665b4d25bed1d18be51a4f681
Ayon Biswas <ayon9901s@gmail.com>
2025-02-14 00:30:44 +0600

    added some operation

---
+++ variable/division.js
@@ -0,0 +1,5 @@
+//division part
+let area=1240;
+let length=12;
+let width=area/length;
+console.log(width);
---
+++ variable/modulus.js
@@ -0,0 +1,5 @@
+//find out the reminder
+let devidend=30;
+let devisor=4;
+let reminder=devidend%devisor;
+console.log(reminder);
