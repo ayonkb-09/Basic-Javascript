45c17d4bcba0cf662795491bdac241506fc7f3f2
Ayon Biswas <ayon9901s@gmail.com>
2025-02-14 22:28:44 +0600

    added comparison

---
+++ Conditional/comparison.js
@@ -0,0 +1,47 @@
+/*
+operator description
+   ==       equal to
+   ===      equal value equal type
+   !=       not equal
+   !==      not equal value not equal type
+   >        greater than
+   <        less than
+   >=       greater than or equal
+   <=       less than or equal      
+*/
+
+//given value
+let x=10;
+//equal to
+console.log(x==18);
+console.log(x==10);
+console.log(x=="10");
+
+//equal value equal type
+
+console.log(x===10);
+console.log(x==="10");
+
+// not equal
+
+console.log(x!=10);
+
+// not equal type not equal value
+
+console.log(x!=="12");
+
+// Greater than
+
+console.log(x>5);
+
+//Greater than or equal
+
+console.log(x>=10);
+
+//less than
+
+console.log(x<=13);
+
+//less than or equal
+
+console.log(x<=13);
